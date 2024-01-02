24b261fab5e5c76360291d2ea390c1a9d2d0389b
TruongThanh <thanh.nguyen.truong@vieon.vn>
2024-01-02 17:32:25 +0700

    #fix layout home page

--- src/sections-page/home/index.tsx
+++ src/sections-page/home/index.tsx
@@ -4,7 +4,7 @@ import PromotionInfo from "./PromotionInfo";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-2 gap-4">
+    <div className="grid lg:grid-cols-2 gap-4">
       <div className="">
         <Banner />
       </div>
